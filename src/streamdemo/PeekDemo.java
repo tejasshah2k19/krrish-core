641b5dc2322aec600db812a1cfec960a0b1124ce
tejas <tejasshah@gmail.com>
2022-08-09 14:42:28 +0530

    peek

---
+++ src/streamdemo/PeekDemo.java
@@ -0,0 +1,31 @@
+package streamdemo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class PeekDemo {
+
+	public static void main(String[] args) {
+
+		List<String> list1 = Arrays.asList("royal", "education", "yellow", "blue");
+		// print all string who's length is > 4
+		list1.stream().filter(x -> x.length() > 4).forEach(x -> System.out.println(x));
+
+		// print all string in uppercase who's length is > 4
+		list1.stream().filter(x -> x.length() > 4).forEach(x -> System.out.println(x.toUpperCase()));
+
+		// print all string in uppercase who's length is > 4 and store into anoter list
+		List<String> strUpper4 = list1.stream().filter(x -> x.length() > 4)
+				.peek(x -> System.out.println(x.toUpperCase()))
+				.map(x->x.toUpperCase())
+				.collect(Collectors.toList());
+		System.out.println(strUpper4);
+		
+		
+		Stream<String>  strStream = Stream.of("blue","red","silver","white","black","purple");
+		
+	}
+}
---
+++ src/streamdemo/SquareOfListData.java
@@ -0,0 +1,18 @@
+package streamdemo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class SquareOfListData {
+
+	public static void main(String[] args) {
+		List<Integer> num = Arrays.asList(1, 3, 4, 6, 7, 9, 10);
+
+		List<Integer> sqr = num.stream().map(n -> n * n).collect(Collectors.toList());
+
+		num.stream().map(n->n*n).forEach(ans->System.out.println(ans));
+		
+	}
+}
